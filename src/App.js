49fc8aba5c220676c8f44a66dfb72429fda0bb79
AbhishekkGautam <abhishekgautm243@gmail.com>
2021-11-27 03:15:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const url = "https://api.twitter.com/2/spaces/search?query=hello";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+
+  function inputHandler(e) {
+    setInput(e.target.value);
+  }
+
+  function btnHandler() {
+    // axios
+    //   .get(url, { headers: { Authorization: `Bearer ${token}` } })
+    //   .then((res) => res.json())
+    //   .then((json) => console.log(json))
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    fetch(url, {
+      headers: { Authentication: `Bearer ${process.env.REACT_APP_TOKEN}` },
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+      .catch((error) => console.log(error));
+  }
+
+  // console.log(`Bearer ${token}`)
+  console.log(`Bearer ${process.env.REACT_APP_TOKEN}`);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :</p>
+      <input type="text" onChange={inputHandler} />
+      <button onClick={btnHandler}>Find Spaces</button>
     </div>
   );
 }

226c40c9d8706206d7d51f1759e1ca03395d6de8
AbhishekkGautam <46835940+AbhishekkGautam@users.noreply.github.com>
2021-12-10 13:27:15 +0530

    Update SpaceCard.js

--- src/components/SpaceCard.js
+++ src/components/SpaceCard.js
@@ -16,7 +16,11 @@ const SpaceCard = ({ spaces, user }) => {
   return (
     <div className="bg-primaryGray rounded-xl">
       <div className="px-6 py-5">
-        <a href={`https://twitter.com/i/spaces/${spaces.id}`}>
+        <a
+          href={`https://twitter.com/i/spaces/${spaces.id}`}
+          target="_blank"
+          rel="noreferrer"
+        >
           <h1 className="text-base font-semibold">{spaces.title}</h1>
           <div className="text-xs font-normal pt-3">
             {spaces.state === "live" ? (
@@ -36,7 +40,11 @@ const SpaceCard = ({ spaces, user }) => {
                 className="px-6 py-5 flex items-center justify-between"
               >
                 <div className="">
-                  <a href={`https://twitter.com/${user.username}`}>
+                  <a
+                    href={`https://twitter.com/${user.username}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <img
                       className="w-10 h-10 rounded-full"
                       src={user.profile_image_url}
@@ -49,7 +57,11 @@ const SpaceCard = ({ spaces, user }) => {
                     <div className="text-sm text-gray-100">Upcoming</div>
                   ) : (
                     <button className="px-4 py-2 text-gray-100 text-sm rounded outline-none bg-primaryGray">
-                      <a href={`https://twitter.com/i/spaces/${spaces.id}`}>
+                      <a
+                        href={`https://twitter.com/i/spaces/${spaces.id}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
                         Join Space
                       </a>
                     </button>
